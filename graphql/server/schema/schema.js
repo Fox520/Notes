e4cb035174db6408daa1df64a17e2fd3d95215d7
Fox <36572168+Fox520@users.noreply.github.com>
2019-10-17 13:37:35 +0200

    start definition of the root query

--- graphql/server/schema/schema.js
+++ graphql/server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require("graphql");
+const _ = require("loadash");
 
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+// dummy data
+var notices = [
+    {topic: 'the first topic', description: 'the first description', id: '1'},
+    {topic: 'the second topic', description: 'the second description', id: '2'},
+    {topic: 'the third topic', description: 'the third description', id: '3'}
+];
 
 const NoticeType = new GraphQLObjectType({
     name: 'Notice',
@@ -11,3 +19,22 @@ const NoticeType = new GraphQLObjectType({
 
     })
 });
+
+const RootQuery = new GraphQLObjectType({
+    // entry to graph
+    name: 'RootQueryType',
+    fields: {
+        notice: {
+            type: NoticeType,
+            args: {id: {type:GraphQLString}},
+            resolve(parent, args){
+                // code to get data from source
+                return _.find(notices, {id: args.id});
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
