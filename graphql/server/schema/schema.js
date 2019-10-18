ccfd28cf8941316d66c20c9273aa7889f662c79d
Fox <36572168+Fox520@users.noreply.github.com>
2019-10-18 14:01:00 +0200

    get notice from random instance

--- graphql/server/schema/schema.js
+++ graphql/server/schema/schema.js
@@ -1,5 +1,6 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const rp = require('request-promise')
 
 const {
     GraphQLObjectType,
@@ -7,26 +8,27 @@ const {
     GraphQLSchema,
     GraphQLInt,
     GraphQLList,
-    GraphQLNonNull} = graphql;
+    GraphQLNonNull } = graphql;
 
 // dummy data
-var notices = [ 
-    {topic: 'the first topic', description: 'the first description', id: '1', day: 10,  weekNumber: 4, month: 8},
-    {topic: 'the second topic', description: 'the second description', id: '2', day: 4, weekNumber: 8, month: 10},
-    {topic: 'the third topic', description: 'the third description', id: '3', day:5, weekNumber: 15, month: 9}
+var notices = [
+    { topic: 'the first topic', description: 'the first description', id: '1', day: 10, weekNumber: 4, month: 8 },
+    { topic: 'the second topic', description: 'the second description', id: '2', day: 4, weekNumber: 8, month: 10 },
+    { topic: 'the third topic', description: 'the third description', id: '3', day: 5, weekNumber: 15, month: 9 }
 ];
 
-const instance_port_pool = [9091, 9092, 9093, 9094];
+var url = "http://192.168.56.101:";
+const instance_port_pool = [9090, 9091, 9092, 9093];
 
 const NoticeType = new GraphQLObjectType({
     name: 'Notice',
     fields: () => ({
-        id: {type: GraphQLInt},
-        topic: {type: GraphQLString},
-        description: {type: GraphQLString},
-        day: {type: GraphQLInt},
-        weekNumber: {type: GraphQLInt},
-        month: {type: GraphQLInt}
+        id: { type: GraphQLInt },
+        topic: { type: GraphQLString },
+        description: { type: GraphQLString },
+        day: { type: GraphQLInt },
+        weekNumber: { type: GraphQLInt },
+        month: { type: GraphQLInt }
     })
 });
 
@@ -36,49 +38,67 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         notice: {
             type: NoticeType,
-            args: {id: {type:GraphQLInt}},
+            args: { id: { type: GraphQLInt } },
             description: 'Return notice of specified id',
-            resolve(parent, args){
-                // code to get data from source
-                return _.find(notices, {id: args.id});
+            resolve(parent, args) {
+                var out = rp(randomAddress() + "/getNotice/" + args.id)
+                    .then(function (str) {
+                        var json = JSON.parse(str)
+                        return json;
+                    })
+                    .catch(function (err) {
+                        // Crawling failed...
+                        console.log(err);
+                        return err;
+                    });
+                return out
             }
         },
         notices: {
             type: new GraphQLList(NoticeType),
             description: 'Returns all notices',
-            resolve(parent, args){
-                // code to get data from source
-                return notices;
+            resolve(parent, args) {
+                var out = rp(randomAddress() + "/getNotices")
+                    .then(function (str) {
+                        var json = JSON.parse(str)
+                        return json;
+                    })
+                    .catch(function (err) {
+                        // Crawling failed...
+                        console.log(err);
+                        return err;
+                    });
+                return out
             }
         },
         // heck, doing this to keep it simple and easier
         dayNotices: {
             type: new GraphQLList(NoticeType),
-            args: {day: {type: GraphQLInt}},
+            args: { day: { type: GraphQLInt } },
             description: 'Return notices of specific day',
-            resolve(parent, args){
+            resolve(parent, args) {
                 // code to get data from source
-                return _.filter(notices, {day: args.day});
+                return _.filter(notices, { day: args.day });
             }
         },
         weekNotices: {
             type: new GraphQLList(NoticeType),
-            args: {weekNumber: {type:GraphQLInt}},
+            args: { weekNumber: { type: GraphQLInt } },
             description: 'Return notices of specific week',
-            resolve(parent, args){
+            resolve(parent, args) {
                 // code to get data from source
-                return _.filter(notices, {weekNumber: args.weekNumber});
+                return _.filter(notices, { weekNumber: args.weekNumber });
             }
         },
         monthNotices: {
             type: new GraphQLList(NoticeType),
-            args: {month: {type:GraphQLInt}},
+            args: { month: { type: GraphQLInt } },
             description: 'Return notices of specific month',
-            resolve(parent, args){
+            resolve(parent, args) {
                 // code to get data from source
-                return _.filter(notices, {month: args.month});
+                return _.filter(notices, { month: args.month });
             }
-        }    
+        }
     }
 })
 
@@ -88,14 +108,14 @@ const Mutation = new GraphQLObjectType({
         addNotice: {
             type: NoticeType,
             args: {
-                id: {type: new GraphQLNonNull(GraphQLInt)},
-                topic: {type: new GraphQLNonNull(GraphQLString)},
-                description: {type: GraphQLString},
-                day: {type: new GraphQLNonNull(GraphQLInt)},
-                weekNumber: {type: new GraphQLNonNull(GraphQLInt)},
-                month: {type: new GraphQLNonNull(GraphQLInt)}
+                id: { type: new GraphQLNonNull(GraphQLInt) },
+                topic: { type: new GraphQLNonNull(GraphQLString) },
+                description: { type: GraphQLString },
+                day: { type: new GraphQLNonNull(GraphQLInt) },
+                weekNumber: { type: new GraphQLNonNull(GraphQLInt) },
+                month: { type: new GraphQLNonNull(GraphQLInt) }
             },
-            resolve(parent, args){
+            resolve(parent, args) {
                 // code to send data to random server instance
                 // ...
                 return args
@@ -104,14 +124,14 @@ const Mutation = new GraphQLObjectType({
         updateNotice: {
             type: NoticeType,
             args: {
-                id: {type: new GraphQLNonNull(GraphQLInt)},
-                topic: {type: GraphQLString},
-                description: {type: GraphQLString},
-                day: {type: GraphQLInt},
-                weekNumber: {type: GraphQLInt},
-                month: {type: GraphQLInt}
+                id: { type: new GraphQLNonNull(GraphQLInt) },
+                topic: { type: GraphQLString },
+                description: { type: GraphQLString },
+                day: { type: GraphQLInt },
+                weekNumber: { type: GraphQLInt },
+                month: { type: GraphQLInt }
             },
-            resolve(parent, args){
+            resolve(parent, args) {
                 // code to send data to random server instance
                 // ...
                 return args
@@ -120,9 +140,9 @@ const Mutation = new GraphQLObjectType({
         removeNotice: {
             type: NoticeType,
             args: {
-                id: {type: new GraphQLNonNull(GraphQLInt)}
+                id: { type: new GraphQLNonNull(GraphQLInt) }
             },
-            resolve(parent, args){
+            resolve(parent, args) {
                 // code to send data to random server instance
                 // ...
                 return args
@@ -130,6 +150,10 @@ const Mutation = new GraphQLObjectType({
         }
     }
 });
+
+function randomAddress() {
+    return url + "9090";//instance_port_pool[Math.floor(Math.random() * instance_port_pool.length)]
+}
 
 module.exports = new GraphQLSchema({
     query: RootQuery,
