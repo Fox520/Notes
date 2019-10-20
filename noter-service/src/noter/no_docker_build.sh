dbeb26ce5bcbafdbfad71bd17c17a82b3a580a47
Fox520 <speedracer520@gmail.com>
2019-10-20 09:49:28 +0100

    improve build script

--- noter-service/src/noter/no_docker_build.sh
+++ noter-service/src/noter/no_docker_build.sh
@@ -1,15 +1,20 @@
 echo "starting build #1"
 ballerina build main.bal
+java -jar main.jar&
 python3 prepare_ports.py
 echo "starting build #2"
 ballerina build main.bal
+java -jar main.jar&
 python3 prepare_ports.py
 echo "starting build #3"
 ballerina build main.bal
+java -jar main.jar&
 python3 prepare_ports.py
 echo "starting build #4"
 ballerina build main.bal
+java -jar main.jar&
 python3 prepare_ports.py
 echo "starting build #5"
 ballerina build main.bal
+java -jar main.jar&
 echo "done"
