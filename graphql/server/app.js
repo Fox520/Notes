feefb708bbb64c63b7fa1d61af61623a9feedb18
Fox <36572168+Fox520@users.noreply.github.com>
2019-10-23 13:14:16 +0200

    allow cross-origin requests

--- graphql/server/app.js
+++ graphql/server/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const cors = require('cors')
 
 const app = express();
+
+// allow cross origin request
+app.use(cors())
 
 app.use("/graphql", graphqlHTTP({
     schema,
